bfa7d7d661c7b1b84c4991d0f9b1b06733c3af36
Alex Kim <45559664+alex-kim-dev@users.noreply.github.com>
2022-08-02 12:09:42 +0500

    refactor: tests

--- src/main.test.js
+++ src/main.test.js
@@ -3,42 +3,53 @@
 const { dscount, checkSyntax } = require('./main');
 
 /**
- * @arg {[any, any][]} cases [arg or args, expected][]
- * @arg {function} fn
+ * @arg {string} testName
+ * @arg {string[]} errors
  * @return {void}
  */
-const testEach = (cases, fn) => {
-  cases.forEach(([args, expected]) => {
-    const callMethod = Array.isArray(args) ? 'apply' : 'call';
-    const actual = fn[callMethod](null, args);
-    console.assert(actual === expected, `${args} -> ${actual}`);
-  });
+const log = (testName, errors) => {
+  if (errors.length === 0) {
+    console.log(`✅ ${testName}`);
+    return;
+  }
+
+  errors.forEach((error) => console.log(`❌ ${testName}: ${error}`));
 };
 
-testEach(
-  [
-    [['ab___ab__', 'a', 'b'], 2],
-    [['___cd____', 'c', 'd'], 1],
-    [['de_______', 'd', 'e'], 1],
-    [['12_12__12', '1', '2'], 3],
-    [['_ba______', 'a', 'b'], 0],
-    [['_a__b____', 'a', 'b'], 0],
-    [['-ab-аb-ab', 'a', 'b'], 2],
-    [['aAa', 'a', 'a'], 2],
-  ],
-  dscount
-);
+/**
+ * @arg {[any, any][]} cases, a tuple of arg / array of args and expected values
+ * @return {(name: string, fn: Function) => void}
+ */
+const testEach = (cases) => (testName, fn) => {
+  const errors = cases.reduce((acc, [args, expected]) => {
+    const callMethod = Array.isArray(args) ? 'apply' : 'call';
+    const actual = fn[callMethod](null, args);
+    return actual === expected
+      ? acc
+      : [...acc, `${args} => ${actual}`];
+  }, new Array());
 
-testEach(
-  [
-    ['---(++++)----', 0],
-    ['', 0],
-    ['before ( middle []) after ', 0],
-    [') (', 1],
-    ['} {', 1],
-    ['<(   >)', 1],
-    ['(  [  <>  ()  ]  <>  )', 0],
-    ['   (      [)', 1],
-  ],
-  checkSyntax
-);
+  log(testName, errors);
+};
+
+testEach([
+  [['ab___ab__', 'a', 'b'], 2],
+  [['___cd____', 'c', 'd'], 1],
+  [['de_______', 'd', 'e'], 1],
+  [['12_12__12', '1', '2'], 3],
+  [['_ba______', 'a', 'b'], 0],
+  [['_a__b____', 'a', 'b'], 0],
+  [['-ab-аb-ab', 'a', 'b'], 2],
+  [['aAa', 'a', 'a'], 2],
+])('consecutive chars', dscount);
+
+testEach([
+  ['---(++++)----', 0],
+  ['', 0],
+  ['before ( middle []) after ', 0],
+  [') (', 1],
+  ['} {', 1],
+  ['<(   >)', 1],
+  ['(  [  <>  ()  ]  <>  )', 0],
+  ['   (      [)', 1],
+])('bracket balancer', checkSyntax);
